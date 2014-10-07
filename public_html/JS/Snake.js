15f67589d06fc3505f2e8d79498f842c3d42e9c8
Hamlet Nortikyan <hamletnortikyan99@gmail.com>
2014-10-07 09:19:20 -0700

    working on snake

---
+++ public_html/JS/Snake.js
@@ -0,0 +1,17 @@
+var snake;
+
+var context;
+var screenwidth;
+var screenheight;
+
+function gameinitalize(){
+    var canvas= document.getelementbyid("game-screen");
+}
+
+function gameloop () {
+    
+}
+
+function gamedraw () {
+    
+}
